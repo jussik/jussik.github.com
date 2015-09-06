1c9d96f37a11f1b999346c9ee2b1836ab74c195e
Jussi Kosunen <deathcarrot@gmail.com>
2015-09-06 20:14:49 +0300

    new tes

--- tes/lib/fetch.js
+++ tes/lib/fetch.js
@@ -7,7 +7,7 @@
 
   function normalizeName(name) {
     if (typeof name !== 'string') {
-      name = String(name)
+      name = name.toString();
     }
     if (/[^a-z0-9\-#$%&'*+.\^_`|~]/i.test(name)) {
       throw new TypeError('Invalid character in header field name')
@@ -17,7 +17,7 @@
 
   function normalizeValue(value) {
     if (typeof value !== 'string') {
-      value = String(value)
+      value = value.toString();
     }
     return value
   }
@@ -200,40 +200,20 @@
     return (methods.indexOf(upcased) > -1) ? upcased : method
   }
 
-  function Request(input, options) {
+  function Request(url, options) {
     options = options || {}
-    var body = options.body
-    if (Request.prototype.isPrototypeOf(input)) {
-      if (input.bodyUsed) {
-        throw new TypeError('Already read')
-      }
-      this.url = input.url
-      this.credentials = input.credentials
-      if (!options.headers) {
-        this.headers = new Headers(input.headers)
-      }
-      this.method = input.method
-      this.mode = input.mode
-      if (!body) {
-        body = input._bodyInit
-        input.bodyUsed = true
-      }
-    } else {
-      this.url = input
-    }
+    this.url = url
 
-    this.credentials = options.credentials || this.credentials || 'omit'
-    if (options.headers || !this.headers) {
-      this.headers = new Headers(options.headers)
-    }
-    this.method = normalizeMethod(options.method || this.method || 'GET')
-    this.mode = options.mode || this.mode || null
+    this.credentials = options.credentials || 'omit'
+    this.headers = new Headers(options.headers)
+    this.method = normalizeMethod(options.method || 'GET')
+    this.mode = options.mode || null
     this.referrer = null
 
-    if ((this.method === 'GET' || this.method === 'HEAD') && body) {
+    if ((this.method === 'GET' || this.method === 'HEAD') && options.body) {
       throw new TypeError('Body not allowed for GET or HEAD requests')
     }
-    this._initBody(body)
+    this._initBody(options.body)
   }
 
   function decode(body) {
@@ -285,6 +265,7 @@
   self.Response = Response;
 
   self.fetch = function(input, init) {
+    // TODO: Request constructor should accept input, init
     var request
     if (Request.prototype.isPrototypeOf(input) && !init) {
       request = input
